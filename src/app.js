c1187cb7d3549cf1c5365c26f35e4a07307d66c5
José Areia <jose.apareia@gmail.com>
2021-11-24 16:27:30 +0000

    edit(app.js): require new modules

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
-import _ from 'loadsh';
 import 'bootstrap';
 import './scss/app.scss';
+
+window.THREE = require('three');
+window.barba = require('@barba/core');
+window.$ = require('jquery');
+window.gsap = require('gsap');
